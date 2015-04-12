9c8cb7e785780903832a9e652c7e89ce5beb6d46
hs105133 <hemant.singh@techmahindra.com>
2015-04-13 01:05:05 +0530

    gh-pages created

--- public/app/scripts/controllers/HomeCtrl.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('userApp')
-	.controller('HomeCtrl', function($scope, $location, $rootScope) {
-   		$scope.$emit("hideNavbar");
-	});
--- public/app/scripts/services/UserService.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-angular.module('userApp')
-    .constant("BASE_URL", "https://hkapi.herokuapp.com/")
-    .factory('UserService', function($resource,BASE_URL) {
-        return $resource(BASE_URL+"msusers/:userId", {userId: "@id"}, { update: { method: 'put'} }); 
-    });
